d4dec6bf1b89182e293ec791c3ea582fe1208681
shybyte <shybyte@users.noreply.github.com>
2023-06-06 08:53:33 +0200

    Add one byte

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 const linux = std.os.linux;
 
 pub export fn _start() callconv(.Naked) noreturn {
-    _ = linux.write(1, "hello", 5);
+    _ = linux.write(1, "Hello\n", 6);
     linux.exit(0);
 }
